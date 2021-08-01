613f22af86d27d3be0b9e7c4f69ad0f26b881644
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-08-01 00:17:58 -0300

    Atividade 9

---
+++ Atividades/A9/main.js
@@ -0,0 +1,79 @@
+// abre e fecha o menu quando clicar no ícone
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//quando clicar em um item do meuni, esconder 
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//Mudar o header da página quando der scroll
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+  
+    if (window.scrollY >= navHeight) {
+      // scroll é maior que a altura do header
+      header.classList.add('scroll')
+    } else {
+      // menor que a altura do header
+      header.classList.remove('scroll')
+    }
+  }
+
+// Testimonials carousel slider swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+  })
+
+// ScrollReveal: Mostrar elementos quando der scroll na pág
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+  )
+  
+
+// Botão voltar para o topo
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+  
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  }
+
+// When Scroll 
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
