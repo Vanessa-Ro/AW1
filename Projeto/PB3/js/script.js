7c8aa6c883a117ff2152d8e0cca1ee05f4b97fde
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-10-23 23:47:15 -0300

    Adicionando JavaScript

---
+++ Projeto/PB3/js/script.js
@@ -0,0 +1,86 @@
+window.addEventListener ('focusout', () => {
+
+    let valueCount = Number(document.getElementById('value').value);
+    let percentage = document.getElementById('tip').value;
+    let people = document.getElementById('people').value;
+
+    if(valueCount < 0 || percentage < 0 || people < 0){
+        let error = document.getElementById('error');
+
+        error.innerHTML = 'Digite apenas números positivos';
+        error.style.display = 'block';
+
+    }
+    else {
+    function calculateTip() { 
+       let totalTipFormat;  
+       let totalTip = ( (valueCount * percentage) / 100);
+       
+       totalTipFormat = totalTip;
+
+       totalTipFormat = totalTip.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+       function tipPeople() {
+
+        let tipPeopleFormat; 
+        let tipPeople = totalTip / people;
+
+        tipPeopleFormat = tipPeople;
+        
+        tipPeopleFormat = tipPeople.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+        function totalPeople() {
+            let totalPeople = valueCount + tipPeople;
+            
+            totalPeople = totalPeople.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL"
+            });
+
+            function total() {
+                let total = valueCount + totalTip;
+
+                total = total.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL"
+                });
+                
+            
+                if (percentage == 0 || people == 0) {
+                    document.getElementById('total-tip').value = "";
+                    document.getElementById('tip-people').value = "";
+                    document.getElementById('total-people').value = "";
+                    document.getElementById('total').value = ""; 
+                }
+                else {
+                    document.getElementById('total-tip').value = totalTipFormat;
+                    document.getElementById('tip-people').value = tipPeopleFormat; 
+                    document.getElementById('total-people').value = totalPeople; 
+                    document.getElementById('total').value = total; 
+                }
+
+            }
+
+            total();
+        }
+
+        totalPeople();
+
+    }
+
+    tipPeople();
+    
+    }
+
+   calculateTip();
+
+   error.style.display = 'none';
+}
+
+})
